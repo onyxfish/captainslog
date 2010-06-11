62c934a6e4e80f9b81b0715f188aadd4634fb41c
cgroskopf <cgroskopf@tribune.com>
2010-06-11 09:00:57 -0500

    Testing with other logs.

--- settings.py
+++ settings.py
@@ -7,8 +7,10 @@ LOG_COLLECTION = 'logs'
 # What logs should be parsed/tracked for analysis?
 USER_LOGS = [
     # (path, type, format)
-    ('/var/log/apache2/access_log', 'apache_access', 'CLF'),
-    ('/Users/sk/src/captainslog/app1/apps.access.log', 'apache_access', 'NCSA'),
+    # ('/var/log/apache2/access_log', 'apache_access', 'CLF'),
+    # ('/Users/sk/src/captainslog/app1/apps.access.log', 'apache_access', 'NCSA'),
+    ('/Users/sk/src/captainslog/app1/homicides.access.log', 'apache_access', '\[?%h\]? %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\" %I %O'),
+    ('/Users/sk/src/captainslog/app1/elections.access.log', 'apache_access', '\[?%h\]? %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\" %I %O'),
 ]
 
 # What columns should be faceted for drill-down navigation?
